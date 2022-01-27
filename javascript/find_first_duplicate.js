4f3b4ff147e5d2babfa50cc7340719cb2b7764f3
Yeison <enmanuelquiterios@gmail.com>
2022-01-27 12:05:23 -0500

    Completed

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -17,3 +17,21 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// 1. I need to find a Number in the array that It's equal to next number
+
+// 2. Test Cases (always Array: Key word Given an Array )
+
+// 3. Pseudocode
+// Need to loop over the array and Check Current Number with Next number Meaning current Number at x index and y Number at x + 1
+
+const find_first_Number = (arr) => {
+  const elementsSet = new set();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (elementsSet.has(arr[i])) return arr[i];
+    elementsSet.add(arr[i]);
+  }
+
+  return -1;
+};
